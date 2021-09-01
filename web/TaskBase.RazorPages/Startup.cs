cbc9fa19c3b3cdeee1ebe05ae73a411ff9091dda
t-rolfin <rolfin.dc@gmail.com>
2021-09-01 13:36:41 +0300

    Added Task page and register infrastructure dependencies in StartUp class.

--- web/TaskBase.RazorPages/Startup.cs
+++ web/TaskBase.RazorPages/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TaskBase.Services;
 
 namespace TaskBase.RazorPages
 {
@@ -24,6 +25,7 @@ namespace TaskBase.RazorPages
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddInfrastructure();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
