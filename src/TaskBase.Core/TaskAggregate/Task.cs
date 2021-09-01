832332522a78c8f567ea11c6499e8b35f0e4137d
t-rolfin <rolfin.dc@gmail.com>
2021-09-01 10:19:35 +0300

    #1 Created logic for Task.

---
+++ src/TaskBase.Core/TaskAggregate/Task.cs
@@ -0,0 +1,43 @@
+﻿using Ardalis.GuardClauses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskBase.Core.Enums;
+using TaskBase.Core.Shared;
+
+namespace TaskBase.Core.TaskAggregate
+{
+    public class Task : Entity<Guid>
+    {
+        public Task(string title, string description, DateTime dueDate)
+            : base(Guid.NewGuid())
+        {
+            Title = Guard.Against.NullOrWhiteSpace(title, nameof(title));
+            Description = description;
+            DueDate = dueDate;
+            CreatedAt = DateTime.Now;
+            TaskState = TaskState.New;
+        }
+
+        public string Title { get; init; }
+        public string Description { get; init; }
+        public DateTime CreatedAt { get; init; }
+        public DateTime CompletedAt { get; protected set; }
+        public DateTime DueDate { get; protected set; }
+        public TaskState TaskState { get; protected set; }
+
+        public void StartWorking()
+        {
+            TaskState = TaskState.InProgress;
+        }
+
+        public void CompleteTask()
+        {
+            TaskState = TaskState.Completed;
+            CompletedAt = DateTime.Now;
+        }
+
+    }
+}
