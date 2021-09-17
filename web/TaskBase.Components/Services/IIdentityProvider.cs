2cb235a9d88c2543c11e7cb928860d5ac0a3ab36
t-rolfin <rolfin.dc@gmail.com>
2021-09-17 11:29:38 +0300

    Added service for providing current user details.

---
+++ web/TaskBase.Components/Services/IIdentityProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskBase.Components.Services
+{
+    public interface IIdentityProvider
+    {
+        string GetCurrentUserIdentity();
+        string GetCurrentUserName();
+    }
+}
---
+++ web/TaskBase.Components/Services/IdentityProvider.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskBase.Components.Services
+{
+    public class IdentityProvider : IIdentityProvider
+    {
+        readonly IHttpContextAccessor _context;
+
+        public IdentityProvider(IHttpContextAccessor context)
+        {
+            _context = context;
+        }
+
+        public string GetCurrentUserIdentity()
+        {
+            return _context.HttpContext.User.Claims.First(x => x.Type.Contains("nameidentifier")).Value;
+        }
+
+        public string GetCurrentUserName()
+        {
+            return _context.HttpContext.User.Identity.Name;
+        }
+    }
+}
