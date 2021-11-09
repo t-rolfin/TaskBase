a7e3db004a5e56075a3d49f8569521d6c3afbcbb
t-rolfin <rolfin.dc@gmail.com>
2021-11-09 13:07:30 +0200

    Modified Program.cs class from API to make testing more easier.

--- API/Program.cs
+++ API/Program.cs
@@ -13,67 +13,73 @@ using TaskBase.Data;
 using TaskBase.Data.Identity;
 using TaskBase.Data.Utils;
 
-var configuration = GetConfiguration();
-var log = LoggerFactory.Create(x => x.AddLog4Net())
-    .CreateLogger<Startup>();
-
-try
+public class Program
 {
-    log.LogInformation("Application Programing Interface is starting...");
-
-    var host = BuildWebHost(configuration, args);
-
-    host.MigrateDbContext<IdentityContext>((context, services) =>
+    public static void Main(string[] args)
     {
-        var env = (IWebHostEnvironment)services.GetService(typeof(IWebHostEnvironment));
-        var logger = (ILogger<IdentityContextSeed>)services.GetService(typeof(ILogger<IdentityContextSeed>));
+        var configuration = GetConfiguration();
+        var log = LoggerFactory.Create(x => x.AddLog4Net())
+            .CreateLogger<Startup>();
 
-        new IdentityContextSeed()
-        .SeedAsync(context, env, logger)
-        .Wait();
-    });
+        try
+        {
+            log.LogInformation("Application Programing Interface is starting...");
 
-    host.MigrateDbContext<TaskContext>((context, services) =>
-    {
-        var identityContext = (IdentityContext)services.GetService(typeof(IdentityContext));
+            var host = CreateWebHostBuilder(args)
+                .ConfigureAppConfiguration(config => GetConfiguration()).Build();
 
-        new TaskContextSeed()
-        .SeedAsync(identityContext, context)
-        .Wait();
-    });
+            host.MigrateDbContext<IdentityContext>((context, services) =>
+            {
+                var env = (IWebHostEnvironment)services.GetService(typeof(IWebHostEnvironment));
+                var logger = (ILogger<IdentityContextSeed>)services.GetService(typeof(ILogger<IdentityContextSeed>));
 
-    host.Run();
-    return 0;
-}
-catch (Exception ex)
-{
-    log.LogError(ex.Message);
-    return 1;
-}
+                new IdentityContextSeed()
+                .SeedAsync(context, env, logger)
+                .Wait();
+            });
 
+            host.MigrateDbContext<TaskContext>((context, services) =>
+            {
+                var identityContext = (IdentityContext)services.GetService(typeof(IdentityContext));
 
-IWebHost BuildWebHost(IConfiguration configuration, string[] args)
+                new TaskContextSeed()
+                .SeedAsync(identityContext, context)
+                .Wait();
+            });
+
+            host.Run();
+        }
+        catch (Exception ex)
+        {
+            log.LogError(ex.Message);
+        }
+    }
+
+    public static IWebHostBuilder CreateWebHostBuilder(string[] args)
     => WebHost.CreateDefaultBuilder(args)
     .CaptureStartupErrors(false)
     .UseStartup<Startup>()
-    .ConfigureAppConfiguration(x => x.AddConfiguration(configuration))
     .UseContentRoot(Directory.GetCurrentDirectory())
     .ConfigureLogging(x =>
     {
         x.ClearProviders();
         x.AddLog4Net();
-    })
-    .Build();
+    });
 
+    public static IConfiguration GetConfiguration()
+    {
+        var builder = new ConfigurationBuilder();
 
-IConfiguration GetConfiguration()
-{
-    var builder = new ConfigurationBuilder();
+        builder
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables();
 
-    builder
-        .SetBasePath(Directory.GetCurrentDirectory())
-        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-        .AddEnvironmentVariables();
-
-    return builder.Build();
+        return builder.Build();
+    }
 }
+
+
+
+
+
