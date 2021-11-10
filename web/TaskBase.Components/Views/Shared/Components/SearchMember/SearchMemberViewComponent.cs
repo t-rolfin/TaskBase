a7efcaf5971d17f9a9ffb39737fd94659eb06ca0
t-rolfin <rolfin.dc@gmail.com>
2021-11-10 11:59:08 +0200

    Implemente search members view component for task assigning.

--- web/TaskBase.Components/Views/Shared/Components/SearchMember/SearchMemberViewComponent.cs
+++ web/TaskBase.Components/Views/Shared/Components/SearchMember/SearchMemberViewComponent.cs
@@ -6,30 +6,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TaskBase.Components.Models;
+using TaskBase.Components.Services;
 
 namespace TaskBase.Components.Views.Shared.Components.SearchMember
 {
     [ViewComponent(Name = "SearchMember")]
     public class SearchMemberViewComponent : ViewComponent
     {
+        private readonly IAuthService _authService;
+
+        public SearchMemberViewComponent(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
         public List<UserModel> Users { get; set; } = new List<UserModel>();
 
 
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            //if (Users is null) Users = new();
+            if (Users is null) Users = new();
 
-            //var users = _userManager.Users.ToList();
-            //foreach (User user in users)
-            //{
-            //if (await _userManager.IsInRoleAsync(user, "Member"))
-            //    Users.Add(new UserModel(
-            //            user.Id,
-            //            user.UserName,
-            //            user.AvatarUrl,
-            //            null
-            //        ));
-            //}
+            Users = await _authService.GetMembersAsync();
 
             return await Task.FromResult(View(Users));
         }
